f9d3b352ad5fbc2653ae937294abcd83707385bb
Daniel  Pan <danielcpan.dev@gmail.com>
2020-09-11 10:22:12 -0700

    added useOS hook

---
+++ src/hooks/useOperatingSystem.ts
@@ -0,0 +1,20 @@
+import { useMemo } from 'react';
+
+const useOperatingSystem = (): string => {
+  return useMemo(() => {
+    const { userAgent } = window.navigator;
+
+    if (userAgent.indexOf('Windows NT 10.0') != -1) return 'Windows 10';
+    if (userAgent.indexOf('Windows NT 6.2') != -1) return 'Windows 8';
+    if (userAgent.indexOf('Windows NT 6.1') != -1) return 'Windows 7';
+    if (userAgent.indexOf('Windows NT 6.0') != -1) return 'Windows Vista';
+    if (userAgent.indexOf('Windows NT 5.1') != -1) return 'Windows XP';
+    if (userAgent.indexOf('Windows NT 5.0') != -1) return 'Windows 2000';
+    if (userAgent.indexOf('Mac') != -1) return 'Mac/iOS';
+    if (userAgent.indexOf('X11') != -1) return 'UNIX';
+    if (userAgent.indexOf('Linux') != -1) return 'Linux';
+    return 'Unknown';
+  }, [window.navigator.userAgent]);
+};
+
+export default useOperatingSystem;
