d12453e80327c6cc492e46a06312d526e3fb7c62
chadchotik <chadchotikasatien@gmail.com>
2020-09-30 10:13:04 -0700

    Added Windows 7 test to useOperatingSystem.test

--- src/hooks/useOperatingSystem/useOperatingSystem.test.tsx
+++ src/hooks/useOperatingSystem/useOperatingSystem.test.tsx
@@ -9,11 +9,17 @@ describe('getOperatingSystem()', () => {
     expect(result).toEqual('Windows 10');
   });
 
-  // TODO: Add Windows 7
+  it('should return Windows 7', () => {
+    const userAgent =
+      'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36';
+    const result = getOperatingSystem(userAgent);
+
+    expect(result).toEqual('Windows 7');
+  });
 
   it('should return unknown', () => {
     const result = getOperatingSystem('im a user agent that does not exist');
 
     expect(result).toEqual('Unknown');
-  });
+  });  
 });
